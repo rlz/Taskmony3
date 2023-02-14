7a158bc917f82deca64dcb5d8687c0858ac2cda7
natalia-a-doskach <natalia.a.doskach@gmail.com>
2023-02-14 16:28:57 +0300

    change assignee in task from direction

--- taskmony-front/src/pages/direction/tasks.tsx
+++ taskmony-front/src/pages/direction/tasks.tsx
@@ -6,28 +6,43 @@ import arrowDown from "../../images/arrow-down.svg";
 import { Task } from "../../components/task";
 import { FilterDivider } from "../../components/filter/filter-divider";
 import { EditedTask } from "../../components/edited/edited-task";
-import { addTask } from "../../services/actions/tasksAPI";
+import {
+  addTask,
+  CHANGE_TASK_DIRECTION,
+} from "../../services/actions/tasksAPI";
 import { useAppDispatch, useAppSelector } from "../../utils/hooks";
 import { FilterByCreator } from "../../components/filter/by-creator";
 
-function Tasks({directionId,directionName}) {
+function Tasks({ directionId, directionName }) {
   const [newTask, setNewTask] = useState(false);
   const task = useAppSelector((store) => store.editedTask);
-  const tasksToShow = useAppSelector((store) => store.tasks.items).filter(t=>t.direction?.id==directionId);
+  const tasksToShow = useAppSelector((store) => store.tasks.items).filter(
+    (t) => t.direction?.id == directionId
+  );
+  const direction = useAppSelector((store) => store.directions.items).filter(d => d.id == directionId)[0];
   useEffect(() => {
     console.log(tasksToShow);
   }, [tasksToShow]);
   const dispatch = useAppDispatch();
   const addANewTask = (direction) => {
-    dispatch(addTask(task,direction));
+    dispatch(addTask(task, direction));
   };
-  const tasks = tasksToShow.map((task) => 
-    <Task task={task} direction={directionName}/>
-  );
+  const tasks = tasksToShow.map((task) => (
+    <Task task={task} direction={directionName} />
+  ));
   return (
     <div className="flex w-full">
       <div className="w-3/4  m-3">
-        <AddBtn label={"add a new task"} onClick={() => setNewTask(true)} />
+        <AddBtn
+          label={"add a new task"}
+          onClick={() => {
+            setNewTask(true);
+            dispatch({
+              type: CHANGE_TASK_DIRECTION,
+              payload: direction,
+            });
+          }}
+        />
         {newTask && (
           <EditedTask
             label={"new task"}
@@ -41,12 +56,12 @@ function Tasks({directionId,directionName}) {
         )}
         {tasks}
       </div>
-      <Filter directionId={directionId}/>
+      <Filter directionId={directionId} />
     </div>
   );
 }
 
-function Filter({directionId}) {
+function Filter({ directionId }) {
   const [isOpen, setIsOpen] = useState<boolean>(true);
   return (
     <div className="w-1/5 mt-4">
@@ -54,8 +69,8 @@ function Filter({directionId}) {
       <FilterItem label="show assigned to me" checked />
       <FilterItem label="show assigned by me" checked />
       <FilterItem label="show followed" checked />
-      <hr/>
-      <FilterByCreator id={directionId}/>
+      <hr />
+      <FilterByCreator id={directionId} />
     </div>
   );
 }

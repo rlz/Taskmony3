a063a1737b02cce5ca0c652de46f4da723b3e1a1
natalia-a-doskach <natalia.a.doskach@gmail.com>
2023-03-10 14:49:42 +0300

    custom select

--- taskmony-front/src/components/edited/date-picker.tsx
+++ taskmony-front/src/components/edited/date-picker.tsx
@@ -23,7 +23,7 @@ export const DatePicker = ({
       </p>
       <input
         type="date"
-        className="font-semibold text-sm text-blue-500 focus:outline-none w-28"
+        className="font-semibold border-none text-sm text-blue-500 focus:outline-none w-28"
         value={new Date(date).toISOString().substring(0, 10)}
         min={
           min
--- taskmony-front/src/components/edited/item-picker.tsx
+++ taskmony-front/src/components/edited/item-picker.tsx
@@ -2,6 +2,7 @@ import arrowUp from "../../images/arrow-up.svg";
 import arrowDown from "../../images/arrow-down.svg";
 import divider from "../../images/divider.svg";
 import { useState } from "react";
+import { Select } from "./select";
 
 type Props = {
   title: string;
@@ -19,24 +20,35 @@ export const ItemPicker = ({
   onChange,
   width,
 }: Props) => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className={"flex justify-between items-center pl-2"}>
+    <div className={"flex justify-between items-center pl-2 relative"}>
       <p className={"font-semibold text-sm text-blue-500"}>{title}:</p>
-      <select
-        name="options"
-        id="options"
-        defaultValue={option}
-        className={
-          "font-semibold text-sm text-blue-500 focus:outline-none " + width
-        }
-        onChange={(e) => onChange(e.target.selectedIndex)}
-      >
-        {options.map((o,index) => (
-          <option value={o} key={index} className={"font-semibold text-sm text-blue-500"}>
-            {o}
-          </option>
-        ))}
-      </select>
+      <div className={"relative  pl-1 pr-1"}>
+        <p
+          className={"font-semibold text-sm text-blue-500 cursor-pointer"}
+          onClick={() => setShowMenu(true)}
+        >
+          {option}
+        </p>
+        {showMenu && (
+          <ul className={"absolute top-0 left-0 bg-white pl-1 pr-1 z-50 h-20 overflow-scroll"}>
+            {options.map((option, index) => (
+              <li
+                className="bg-white z-50 cursor-pointer select-none hover:underline font-semibold text-sm text-blue-500"
+                onClick={(e) => {
+                  setShowMenu(false);
+                  onChange(index);
+                }}
+                key={index}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
       {hasBorder && <img src={divider}></img>}
     </div>
   );
